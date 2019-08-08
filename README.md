38d1ead2657617dd729ad01b7296e42b53ba539e
rongfengliang <1141591465@qq.com>
2019-08-08 16:07:46 +0800

    init project for ulid test

---
+++ README.md
@@ -0,0 +1,19 @@
+# ulid nodejs learning
+
+> just test id conflicts
+
+## how to running
+
+> use npm-run-all && yarn
+
+* with yarn
+
+```code
+yarn -s run-all
+```
+
+* with shell
+
+```code
+./fire
+```
---
+++ app.js
@@ -0,0 +1,10 @@
+const ulid = require("@kiosked/ulid")
+for (let index = 0; index < 1000000; index++) {
+    let id = ulid.ulid()
+    console.log(id)
+}
+// let tempid = "01DHQX1VCKA258WSYT46JGCW3P"
+
+// let gettime = ulid.decodeTime(tempid)
+
+// console.log(gettime)
